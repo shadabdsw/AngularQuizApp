3d0dfe928103a214bbd8f6dcdc1274b2eb89cb6f
shadabdsw <shadab1996@gmail.com>
2021-09-01 13:33:26 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { service } from './service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'quizApp';
+  quiz: Observable<any>;
+  errorMessage='';
+  public isCollapsed = false;
+  constructor(private quizques: service) {
+     this.quiz = this.quizques.getQuiz();
+    }
+  
+}
